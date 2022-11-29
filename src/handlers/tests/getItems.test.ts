ddba564570fb5ac2bdb8508d43b177672863cbcc
krishnarao inturi <krishnarao.inturi@technoidentity.com>
2022-11-29 07:19:38 +0530

    MOONY-116:update get all item api

--- src/handlers/tests/getItems.test.ts
+++ src/handlers/tests/getItems.test.ts
@@ -94,7 +94,7 @@ describe('Items list', () => {
         it('Should return 200 when filters applied', async () => {
             const result = await request(app)
                 .get(
-                    '/admins/items?itemSubTypeId=1&itemSubTypeId=2&attireTypeId=1&attireTypeId=2&wearTypeId=1&wearTypeId=2&seasonId=1&seasonId=2&colorId=1&colorId=2&fitId=1&fitId=2&waistLocationId=1&waistLocationId=2&keywordId=1&keywordId=2&clothSizeId=1&clothSizeId=2&shoeSizeId=1&shoeSizeId=2&shoeHeightId=1&shoeHeightId=2&jewelryTypeId=1&jewelryTypeId=2',
+                    '/admins/items?itemTypeId=1&itemTypeId=2&itemSubTypeId=2&&itemSubTypeId=3&attireTypeId=1&attireTypeId=2&wearTypeId=1&wearTypeId=2&seasonId=1&seasonId=2&colorId=1&colorId=2&fitId=1&fitId=2&waistLocationId=1&waistLocationId=2&keywordId=1&keywordId=2&clothSizeId=1&clothSizeId=2&shoeSizeId=1&shoeSizeId=2&shoeHeightId=1&shoeHeightId=2&jewelryTypeId=1&jewelryTypeId=2',
                 )
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `bearer ${accessToken}`);
